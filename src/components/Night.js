61e1e2480b5d18fba7f1a415061c4896815dc2d3
witchicken <witchicken2@gmail.com>
2022-10-17 16:34:44 +0900

    ing-night-threejs

--- src/components/Night.js
+++ src/components/Night.js
@@ -1,8 +1,11 @@
 import "./css/Night.scss";
+import { ReactComponent as Man } from "../svg/man.svg";
 function Night() {
   return (
     <div className="NightCompo">
-      <div className="innerCircle"></div>
+      <div className="innerCircle">
+        <Man width="110" height="110" />
+      </div>
     </div>
   );
 }
--- src/components/Sunny.js
+++ src/components/Sunny.js
@@ -5,6 +5,7 @@ import { ReactComponent as Grass } from "../svg/grass.svg";
 import { ReactComponent as WateringCan } from "../svg/wateringCan.svg";
 import { ReactComponent as Flower } from "../svg/flower.svg";
 import { ReactComponent as Tree } from "../svg/tree.svg";
+import { ReactComponent as Water } from "../svg/water.svg";
 
 function Sunny() {
   return (
@@ -47,9 +48,18 @@ function Sunny() {
           <Grass />
         </div>
         <WateringCan className="wateringcan" width="200" height="200" />
+        <Tree className="tree" width="300" height="300" />
+        <Flower className="flower flw1" width="100" height="100" />
+        <Flower className="flower flw2" width="100" height="100" />
+        <Flower className="flower flw3" width="100" height="100" />
+
+        <Water className="water water1" width="30" height="30" fill="blue" />
+        <Water className="water water2" width="30" height="30" fill="blue" />
+        <Water className="water water3" width="30" height="30" fill="blue" />
+        <Water className="water water4" width="30" height="30" fill="blue" />
+        <Water className="water water5" width="30" height="30" fill="blue" />
+        <Water className="water water6" width="30" height="30" fill="blue" />
       </div>
-      <Tree />
-      <Flower />
     </div>
   );
 }
